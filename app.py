870ebb008db4982fc1236d80255c38bbd9b52449
unknown <goldsteinmaxwellmassie@gmail.com>
2025-05-31 23:05:14 +0700

    push app flask

--- app.py
+++ app.py
@@ -79,4 +79,8 @@ def index():
     return "Skin Disease Detection API with details is running."
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
